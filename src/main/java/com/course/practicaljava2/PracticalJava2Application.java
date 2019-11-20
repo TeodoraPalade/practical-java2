4b43d02967505e937702013fc3f5bc8a0227ed25
TeodoraPalade <paladeteodore1@gmail.com>
2019-11-20 15:31:06 +0200

    Initial commit

---
+++ src/main/java/com/course/practicaljava2/PracticalJava2Application.java
@@ -0,0 +1,18 @@
+package com.course.practicaljava2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticalJava2Application /* for deploy war - extends SpringBootServletInitializer */ {
+// for deploy war - 
+//	@Override
+//	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+//		return builder.sources(PracticalJava2Application.class);
+//	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticalJava2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/course/practicaljava2/PracticalJava2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.course.practicaljava2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticalJava2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
